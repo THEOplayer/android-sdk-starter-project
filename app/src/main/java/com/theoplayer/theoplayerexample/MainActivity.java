db0205f330009febe0cf8b8a29d600e453445926
Daniel Dallos <daniel.dallos@theoplayer.com>
2018-04-26 11:35:01 +0200

    Add THEOplayer to the MainActivity

--- app/src/main/java/com/theoplayer/theoplayerexample/MainActivity.java
+++ app/src/main/java/com/theoplayer/theoplayerexample/MainActivity.java
@@ -1,13 +1,20 @@
 package com.theoplayer.theoplayerexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.theoplayer.android.api.THEOplayerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    THEOplayerView theoPlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        theoPlayerView = findViewById(R.id.theoplayer);
+
     }
 }
